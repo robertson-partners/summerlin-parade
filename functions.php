d3369e9f979089d7146b6e44923c66da8a5eeaa1
Tom Madrid <madrid1979@gmail.com>
2022-01-28 11:02:46 -0800

    enqueue fireworks script

--- functions.php
+++ functions.php
@@ -10,3 +10,10 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+function add_fireworks() {
+  if( is_front_page() ) {
+    wp_enqueue_script( 'fireworks-js', get_stylesheet_directory_uri() . '/js/fireworks.js', [], '1.0' );
+  }
+}
+add_action( 'wp_enqueue_scripts', 'add_fireworks', 20 );
