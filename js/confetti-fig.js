fdd7166503dbeacfff36a0bd5c6041db95459dcb
Tom Madrid <madrid1979@gmail.com>
2022-02-02 13:03:41 -0800

    Reduce confetti particles

--- js/confetti-fig.js
+++ js/confetti-fig.js
@@ -1,18 +1,18 @@
 var end = Date.now() + (15 * 1000);
-var colors = ['#d5202f', '#47c0ef', '#154b9d', '#edbc1c', '#ffffff'];
+var colors = ['#d5202f', '#47c0ef', '#154b9d', '#ffffff'];
 var canvas = document.getElementById('confetti-canvas');
 canvas.confetti = canvas.confetti || confetti.create(canvas, { resize: true });
 
 (function frame() {
   confetti({
-    particleCount: 5,
+    particleCount: 4,
     angle: 60,
     spread: 55,
     origin: { x: 0 },
     colors: colors
   });
   confetti({
-    particleCount: 5,
+    particleCount: 4,
     angle: 120,
     spread: 55,
     origin: { x: 1 },
