c6b8443070bada5f9a0bdde84abdbd703430b020
Tom Madrid <madrid1979@gmail.com>
2022-02-02 16:10:24 -0800

    Clean up files and enable editor...
    on the user activation page.

--- functions.php
+++ functions.php
@@ -11,8 +11,10 @@ function avada_lang_setup() {
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
+/* Homepage Confetti Generator */
 function add_confetti() {
   if( is_front_page() ) {
+    /* https://github.com/catdad/canvas-confetti */
     wp_enqueue_script( 'confetti-js', get_stylesheet_directory_uri() . '/js/confetti.browser.min.js', [], '1.4.0' );
     wp_enqueue_script( 'confetti-config-js', get_stylesheet_directory_uri() . '/js/confetti-fig.js', ['confetti-js'], '1.0', true);
   }
@@ -30,3 +32,11 @@ if ( class_exists("GFForms") ){
     'min_validation_message' => __( 'Oops! Your description needs to be at least %s characters.' )
   ));
 }
+
+/**
+ * Gravity Perks // GP Better User Activation // Enable Editor on Activation Page
+ * http://gravitywiz.com/documentionat/gravity-forms-better-user-activation/
+ */
+add_action( 'init', function() {
+	remove_action( 'admin_head', array( gp_better_user_activation(), 'remove_default_content_editor' ) );
+} );
